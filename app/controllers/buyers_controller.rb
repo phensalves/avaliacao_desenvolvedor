28b3ba0cf295eda4a4781aa61a323505f2db4c70
Pedro Alves <pedro_alves@me.com>
2016-04-16 02:42:47 -0300

    scaffold of buyers and suppliers

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,60 @@
+class BuyersController < ApplicationController
+  before_action :set_buyer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @buyers = Buyer.all
+  end
+
+  def show
+  end
+
+  def new
+    @buyer = Buyer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @buyer = Buyer.new(buyer_params)
+
+    respond_to do |format|
+      if @buyer.save
+        format.html { redirect_to @buyer, notice: 'Buyer was successfully created.' }
+        format.json { render :show, status: :created, location: @buyer }
+      else
+        format.html { render :new }
+        format.json { render json: @buyer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @buyer.update(buyer_params)
+        format.html { redirect_to @buyer, notice: 'Buyer was successfully updated.' }
+        format.json { render :show, status: :ok, location: @buyer }
+      else
+        format.html { render :edit }
+        format.json { render json: @buyer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @buyer.destroy
+    respond_to do |format|
+      format.html { redirect_to buyers_url, notice: 'Buyer was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_buyer
+      @buyer = Buyer.find(params[:id])
+    end
+
+    def buyer_params
+      params.require(:buyer).permit(:name)
+    end
+end
---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,62 @@
+class SuppliersController < ApplicationController
+  before_action :set_supplier, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def show
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def edit
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    respond_to do |format|
+      if @supplier.save
+        format.html { redirect_to @supplier, notice: 'Supplier was successfully created.' }
+        format.json { render :show, status: :created, location: @supplier }
+      else
+        format.html { render :new }
+        format.json { render json: @supplier.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @supplier.update(supplier_params)
+        format.html { redirect_to @supplier, notice: 'Supplier was successfully updated.' }
+        format.json { render :show, status: :ok, location: @supplier }
+      else
+        format.html { render :edit }
+        format.json { render json: @supplier.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @supplier.destroy
+    respond_to do |format|
+      format.html { redirect_to suppliers_url, notice: 'Supplier was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+   
+    def set_supplier
+      @supplier = Supplier.find(params[:id])
+    end
+
+    
+    def supplier_params
+      params.require(:supplier).permit(:name, :address)
+    end
+end
